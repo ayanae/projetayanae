785217628bde5bbfe3391639fd52071268bcbed7
ayanae <32362011+ayanae@users.noreply.github.com>
2017-10-09 16:59:35 +0200

    Update wild.php

--- wild.php
+++ wild.php
@@ -1,1 +1,3 @@
-J'edite moi-meme mon propre doc pour faire genre je ne travaille pas seule, genre j'ai des amis quoi... Qui veut un Curly ?
+J'edite moi-meme mon propre doc pour faire genre je ne travaille pas seule, genre j'ai des amis quoi...  
+
+................... Qui veut un Curly ?
